d7b7bb6725f827ec18be439fee7e4c5052ed4021
오재웅 <markjmind@gmail.com>
2015-07-01 10:34:08 +0900

    sliding menu sample 추가

---
+++ sample/sldingmenu/src/com/markjmind/sample/propose/slidingmenu/MainActivity.java
@@ -0,0 +1,54 @@
+package com.markjmind.sample.propose.slidingmenu;
+
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.markjmind.propose.Propose;
+import com.markjmind.propose.Propose.ProposeListener;
+
+public class MainActivity extends Activity{
+	private LinearLayout sliding_lyt;
+	Propose propose;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+		/** Layout init **/
+		sliding_lyt = (LinearLayout)findViewById(R.id.sliding_lyt);
+		sliding_lyt.setClickable(true);
+		
+		 
+		/** Animator create **/
+		float start = -200*Propose.getDensity(this);
+		float end = 0;
+		sliding_lyt.setX(start);
+		ObjectAnimator rightAnimator = ObjectAnimator.ofFloat(sliding_lyt,View.TRANSLATION_X,start,end);
+		rightAnimator.setDuration(500); /** "duration" use to onClick **/
+		
+		/** Propose create **/
+		propose = new Propose(this); 
+		propose.motionRight.play(rightAnimator); /** set Animator **/
+		sliding_lyt.setOnTouchListener(propose); /** set touch listener **/
+		
+		propose.motionRight.setMotionDistance(-start); /** set Drag Distance **/		
+		
+		/**black Alpha**/
+		propose.setOnMotionListener(new ProposeListener() {
+			@Override
+			public void onStart() {}
+			@Override
+			public void onScroll(int Direction, long currDuration, long totalDuration) {
+				float alpha = (float)currDuration/totalDuration/2;
+				findViewById(R.id.alpha_lyt).setAlpha(alpha); // max 0.5
+			}
+			@Override
+			public void onEnd() {}
+		});
+	}
+	
+	
+}
