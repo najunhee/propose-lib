49fab2042a8a607bd19a0a534e9d24966802ed59
오재웅 <markjmind@gmail.com>
2015-07-02 16:43:00 +0900

    motionInitor로 초기화

--- sample/SlidingDrawer/src/com/markjmind/sample/propose/slidingdrawer/MainActivity.java
+++ sample/SlidingDrawer/src/com/markjmind/sample/propose/slidingdrawer/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.markjmind.propose.MotionInitor;
 import com.markjmind.propose.Propose;
 import com.markjmind.propose.Propose.ProposeListener;
 
@@ -20,7 +21,8 @@ public class MainActivity extends Activity{
 	private FrameLayout lyt;
 	private TextView text;
 	private ImageView bg1;
-	
+	private ObjectAnimator rightAnimator;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,37 +31,42 @@ public class MainActivity extends Activity{
 		text = (TextView)findViewById(R.id.text);
 		bg1 = (ImageView)findViewById(R.id.bg1);  
 		
+		text.setVisibility(View.GONE);
+		rightAnimator = ObjectAnimator.ofFloat(lyt,View.TRANSLATION_Y,0,0);
+		rightAnimator.setDuration(1000);
 		
-		bg1.post(new Runnable() {
+		Propose propose = new Propose(getApplicationContext());
+		propose.motionDown.play(rightAnimator);
+		lyt.setOnTouchListener(propose);
+		
+		
+		propose.setMotionInitor(new MotionInitor() {
 			@Override
-			public void run() {
-				text.setVisibility(View.GONE);
+			public void touchDown(Propose propose) {
 				float height = lyt.getHeight()-50*Propose.getDensity(getApplicationContext());
-				ObjectAnimator rightAnimator = ObjectAnimator.ofFloat(lyt,View.TRANSLATION_Y, 0,height);
-				rightAnimator.setDuration(1000);
-				
-				Propose propose = new Propose(getApplicationContext());
-				propose.motionDown.play(rightAnimator,(int)height);
-				lyt.setOnTouchListener(propose);
-				 
-				propose.setOnMotionListener(new ProposeListener() {
-					@Override
-					public void onStart() {
-						text.setVisibility(View.VISIBLE);
-						text.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
-					}
-					@Override
-					public void onScroll(int Direction, long currDuration, long totalDuration) {
-						text.setText(currDuration*100/totalDuration+"%");
-						float alpha = (float)(totalDuration-currDuration*2)/(float)totalDuration; 
-						bg1.setAlpha(alpha);
-					}
-					@Override
-					public void onEnd() {
-						text.setVisibility(View.GONE);
-						text.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
-					}
-				});
+				rightAnimator.setFloatValues(0,height);
+				propose.motionDown.setMotionDistance(height);
+			}
+			@Override
+			public void touchUp(Propose arg0) {}
+		});
+		 
+		propose.setOnMotionListener(new ProposeListener() {
+			@Override
+			public void onStart() {
+				text.setVisibility(View.VISIBLE);
+				text.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+			}
+			@Override
+			public void onScroll(int Direction, long currDuration, long totalDuration) {
+				text.setText(currDuration*100/totalDuration+"%");
+				float alpha = (float)(totalDuration-currDuration*2)/(float)totalDuration; 
+				bg1.setAlpha(alpha);
+			}
+			@Override
+			public void onEnd() {
+				text.setVisibility(View.GONE);
+				text.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
 			}
 		});
 	}
