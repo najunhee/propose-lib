e867e60278826468eb419d194aa79785217e2284
오재웅 <markjmind@gmail.com>
2015-07-06 13:41:08 +0900

    moving view 프로젝트 준비

---
+++ sample/moving_view/src/com/markjmind/sample/propose/moving/MainActivity.java
@@ -0,0 +1,18 @@
+package com.markjmind.sample.propose.moving;
+
+import com.markjmind.sample.propose.paper.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+	}
+	
+	
+}
