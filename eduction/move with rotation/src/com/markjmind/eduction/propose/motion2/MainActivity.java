b593212fbc1258a8c5c3b2beb2314244af820174
오재웅 <markjmind@gmail.com>
2015-08-12 17:02:41 +0900

    move with rotation 예제 준비 완료

---
+++ eduction/move with rotation/src/com/markjmind/eduction/propose/motion2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.markjmind.eduction.propose.motion2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+	}
+	
+	
+}
